d7930fc4c39378c95b7c80c87462d2a5bf0cb33d
Nathan Carnes <nathan@carnesmedia.com>
2011-04-17 20:53:15 -0700

    Cleaning up warnings with Theme Check

--- functions.php
+++ functions.php
@@ -4,8 +4,6 @@
  * @subpackage Bedrock
  */
 
-automatic_feed_links();
-
 if ( function_exists('register_sidebar') ) {
 	register_sidebar(array(
 		'before_widget' => '<li id="%1$s" class="widget %2$s">',
@@ -15,16 +13,20 @@ if ( function_exists('register_sidebar') ) {
 	));
 }
 
-if (function_exists('add_theme_support')) {
+if (function_exists('add_theme_support')) {  
+  add_theme_support('automatic-feed-links');
 	add_theme_support('nav-menus');
 	add_theme_support('post-thumbnails');
 	set_post_thumbnail_size(50, 50, true);
 	add_image_size('single-post-thumbnail', 630, 300, true);
-}
+}                   
 
-add_action( 'init', 'register_menus' );
+if ( ! isset( $content_width ) ) $content_width = 600;
 
-function register_menus() {
+
+add_action( 'init', 'bedrock_register_menus' );
+
+function bedrock_register_menus() {
 	register_nav_menus(
 		array(
 			'header-menu' => __( 'Header Menu' ),
