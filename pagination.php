a96358037b6f9018182f662a3bd16627e840f018
Nathan Carnes <nathan@carnesmedia.com>
2012-01-31 09:25:45 -0800

    Adding $cap global to sidebar.php; fixing typo in pagination.php
    Resolves issues #1 and #2

--- pagination.php
+++ pagination.php
@@ -1,7 +1,7 @@
-<div clas="pagination">
+<div class="pagination">
   <?php global $cap; ?>
   <?php if ( $wp_query->max_num_pages > 1 ) : ?>
-  		<?php next_posts_link( $cap->older_posts_link ); ?>
-  		<?php previous_posts_link( $cap->newer_posts_link ); ?>
+      <?php next_posts_link( $cap->older_posts_link ); ?>
+      <?php previous_posts_link( $cap->newer_posts_link ); ?>
   <?php endif; ?>
 </div>
--- sidebar.php
+++ sidebar.php
@@ -7,26 +7,27 @@
  * @since Bedrock 2.0
  */
 ?>
+<?php global $cap; ?>
 
-			<ul class="xoxo">
+<ul class="widgets">
 
 <?php
-	/* When we call the dynamic_sidebar() function, it'll spit out
-	 * the widgets for that widget area. If it instead returns false,
-	 * then the sidebar simply doesn't exist, so we'll hard-code in
-	 * some default sidebar stuff just in case.
-	 */
-	if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
-	
-			<li>
-				<?php get_search_form(); ?>
-			</li>
+  /* When we call the dynamic_sidebar() function, it'll spit out
+   * the widgets for that widget area. If it instead returns false,
+   * then the sidebar simply doesn't exist, so we'll hard-code in
+   * some default sidebar stuff just in case.
+   */
+  if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
 
-			<li>
-				<h3><?php echo $cap->archives_title; ?></h3>
-				<ul>
-					<?php wp_get_archives( 'type=monthly' ); ?>
-				</ul>
-			</li>
-		<?php endif; // end primary widget area ?>
-			</ul>
+      <li>
+        <?php get_search_form(); ?>
+      </li>
+
+      <li>
+        <h4><?php echo $cap->archives_title; ?></h4>
+        <ul>
+          <?php wp_get_archives( 'type=monthly' ); ?>
+        </ul>
+      </li>
+    <?php endif; // end primary widget area ?>
+</ul>
